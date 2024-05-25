ebe5f7cd980194dcc00cb6764b3eacd6356dc842
Nayan1234l <ali15-13943@diu.edu.bd>
2024-05-25 13:16:13 +0600

    explore-menu-item

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -6,6 +6,12 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import search_icon from './search_icon.png'
 import basket_icon from './basket_icon.png'
+import menu_1 from './menu_1.png'
+import menu_2 from './menu_2.png'
+import menu_3 from './menu_3.png'
+import menu_4 from './menu_4.png'
+import menu_5 from './menu_5.png'
+import menu_6 from './menu_6.png'
 
 export const assets = {
     logo,
@@ -16,6 +22,38 @@ export const assets = {
     upload_area,
     parcel_icon,
     basket_icon,
+    menu_1,
+    menu_2,
+    menu_3,
+    menu_4,
+    menu_5,
+    menu_6,
 }
+export const menu_list = [
+    {
+        menu_name: "Salad",
+        menu_image: menu_1
+    },
+    {
+        menu_name: 'Rolls',
+        menu_image: menu_2
+    },
+    {
+        menu_name: 'Deserts',
+        menu_image: menu_3
+    },
+    {
+        menu_name: 'Sandwich',
+        menu_image: menu_4
+    },
+    {
+        menu_name: 'Cake',
+        menu_image: menu_5
+    },
+    {
+        menu_name: 'Rice',
+        menu_image: menu_6
+    },
+]
 
 export const url = 'http://localhost:4000'
