85e5e19164934178a1428a8dae26c874a2646565
Nayan1234l <ali15-13943@diu.edu.bd>
2024-05-25 11:22:06 +0600

    home-section-completed

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Choose your favourite Lunch here</h2>
+                <p>For all the hardworking day, You deserve this delicious Lunch.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
