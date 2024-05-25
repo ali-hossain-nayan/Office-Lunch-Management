22b656064f55215905d42ce3b0dd175f74cfa005
Nayan1234l <ali15-13943@diu.edu.bd>
2024-05-25 13:56:46 +0600

    create-context-provider-to-access-food-list

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -12,6 +12,38 @@ import menu_3 from './menu_3.png'
 import menu_4 from './menu_4.png'
 import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
+import food_1 from './food_1.png'
+import food_2 from './food_2.png'
+import food_3 from './food_3.png'
+import food_4 from './food_4.png'
+import food_5 from './food_5.png'
+import food_6 from './food_6.png'
+import food_7 from './food_7.png'
+import food_8 from './food_8.png'
+import food_9 from './food_9.png'
+import food_10 from './food_10.png'
+import food_11 from './food_11.png'
+import food_12 from './food_12.png'
+import food_13 from './food_13.png'
+import food_14 from './food_14.png'
+import food_15 from './food_15.png'
+import food_16 from './food_16.png'
+import food_17 from './food_17.png'
+import food_18 from './food_18.png'
+import food_19 from './food_19.png'
+import food_20 from './food_20.png'
+import food_21 from './food_21.png'
+import food_22 from './food_22.png'
+import food_23 from './food_23.png'
+import food_24 from './food_24.png'
+import food_25 from './food_25.png'
+import food_26 from './food_26.png'
+import food_27 from './food_27.png'
+import food_28 from './food_28.png'
+import food_29 from './food_29.png'
+import food_30 from './food_30.png'
+import food_31 from './food_31.png'
+import food_32 from './food_32.png'
 
 export const assets = {
     logo,
@@ -53,6 +85,232 @@ export const menu_list = [
     {
         menu_name: 'Rice',
         menu_image: menu_6
+    },
+]
+export const food_list = [
+    {
+        _id: '1',
+        name: 'salad',
+        image: food_1,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '2',
+        name: 'masala salad',
+        image: food_2,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '3',
+        name: 'healthy salad',
+        image: food_3,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '4',
+        name: 'vegetable',
+        image: food_4,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '5',
+        name: 'Rolls',
+        image: food_5,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'Rolls'
+    },
+    {
+        _id: '6',
+        name: 'salad',
+        image: food_6,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '7',
+        name: 'sorma',
+        image: food_7,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '8',
+        name: 'chicken rolls',
+        image: food_8,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '9',
+        name: 'panir',
+        image: food_9,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '10',
+        name: 'sweet',
+        image: food_10,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '11',
+        name: 'stobery juice',
+        image: food_11,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '12',
+        name: 'malai',
+        image: food_12,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '13',
+        name: 'sandwich',
+        image: food_13,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '14',
+        name: 'salad',
+        image: food_14,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '15',
+        name: 'beef sandwich',
+        image: food_15,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '16',
+        name: 'salad',
+        image: food_16,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '17',
+        name: 'salad',
+        image: food_17,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '18',
+        name: 'salad',
+        image: food_18,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '19',
+        name: 'salad',
+        image: food_19,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '20',
+        name: 'salad',
+        image: food_20,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '21',
+        name: 'salad',
+        image: food_21,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '22',
+        name: 'salad',
+        image: food_22,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '23',
+        name: 'salad',
+        image: food_23,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '24',
+        name: 'salad',
+        image: food_24,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '25',
+        name: 'salad',
+        image: food_25,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '26',
+        name: 'salad',
+        image: food_26,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '27',
+        name: 'salad',
+        image: food_27,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '28',
+        name: 'salad',
+        image: food_28,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '29',
+        name: 'salad',
+        image: food_29,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '30',
+        name: 'salad',
+        image: food_30,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '31',
+        name: 'pasta',
+        image: food_31,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
+    },
+    {
+        _id: '32',
+        name: ' veg salad',
+        image: food_32,
+        description: 'Food provides essential nutrients for overall wll-being',
+        category: 'salad'
     },
 ]
 
