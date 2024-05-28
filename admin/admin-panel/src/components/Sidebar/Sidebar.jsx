f66ea876efffb071e3ad8f67229cf41515bef200
Nayan1234l <ali15-13943@diu.edu.bd>
2024-05-28 15:38:35 +0600

    admin-sidebar-completed

---
+++ admin/admin-panel/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Sidebar.css'
+import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div className="sidebar-options">
+                <NavLink to='/add' className="sidebar-option">
+                    <img src={assets.add_icon} alt="add-icon" />
+                    <p>Add Menu</p>
+                </NavLink>
+                <NavLink to='/list' className="sidebar-option">
+                    <img src={assets.order_icon} alt="add-icon" />
+                    <p>List Menu</p>
+                </NavLink>
+                <NavLink to='/view' className="sidebar-option">
+                    <img src={assets.add_icon} alt="add-icon" />
+                    <p>View Menu</p>
+                </NavLink>
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
